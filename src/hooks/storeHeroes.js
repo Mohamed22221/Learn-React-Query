8f24651a787c33241777578bf43309e54b80e7ed
unknown <hamdymohame7@gmail.com>
2023-02-28 17:00:27 +0200

    details data in localstorage query

--- src/hooks/storeHeroes.js
+++ src/hooks/storeHeroes.js
@@ -1,12 +1,26 @@
 import axios from "axios";
-import { useQuery } from "react-query";
-
+import { useQuery, useQueryClient } from "react-query";
 
 export const StoreHeroes = (key) => {
   return useQuery(key, () => axios.get("http://localhost:4000/posts"));
 };
 
-export const StoreHero = (key , id) => {
-    return useQuery([key , id], () => axios.get(`http://localhost:4000/posts/${id}`));
-  };
-  
+export const StoreHero = (key, id) => {
+  const queryClient = useQueryClient();
+  return useQuery(
+    [key, id],
+    () => axios.get(`http://localhost:4000/posts/${id}`),
+    {
+      initialData: () => {
+        const post = queryClient
+          .getQueryData("posts")
+          ?.data.find((post) => post.id === parseInt(id));
+        if (post) {
+          return { data: post };
+        } else {
+          return undefined;
+        }
+      },
+    }
+  );
+};
